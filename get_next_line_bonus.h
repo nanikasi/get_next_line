cbea8ed885d0a804312ccca850d370e49d7a90f4
Naoto Nakanishi <48846406+nanikasi@users.noreply.github.com>
2022-11-15 00:05:52 +0900

    init:bonus

---
+++ get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nanakani <nanakani@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/10 19:03:49 by nanakani          #+#    #+#             */
+/*   Updated: 2022/11/10 19:03:49 by nanakani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stddef.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strnchr(const char *s, int c, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+void	*ft_memset(void *b, int c, size_t len);
+size_t	ft_strlen(const char *s);
+
+#endif
